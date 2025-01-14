cd0e402c283cb3e92fb63b803c0f19035808a1a0
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2025-01-14 15:24:27 -0300

    refactor: changing the icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'curriculo';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    this.addToggleFunctionality('projects-title', 'project-content');
+    this.addToggleFunctionality('voluntariado-title', 'voluntariado-content');
+    this.addToggleFunctionality('formacao-title', 'formacao-content');
+  }
+
+  addToggleFunctionality(titleId: string, contentId: string): void {
+    const title = document.getElementById(titleId);
+    const content = document.getElementById(contentId);
+
+    if (title && content) {
+      content.style.display = 'none';  // Inicialmente escondido
+      title.addEventListener('click', () => {
+        // Alterna a visibilidade do conteúdo
+        if (content.style.display === 'none') {
+          content.style.display = 'block';
+        } else {
+          content.style.display = 'none';
+        }
+      });
+    }
+  }
 }
